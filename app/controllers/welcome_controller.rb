fe842a6667bba2d90ac505462462789fccd1dcd1
massa <zoumaniguimassa12@gmail.com>
2023-08-08 09:18:21 +0000

    Fixing linter

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
-class WelcomeController < ApplicationController 
- # before_action :redirect_if_signed_in, only: [:index]
-
- def index
-   # Your existing code for the welcome page
- end
+class WelcomeController < ApplicationController
+  # before_action :redirect_if_signed_in, only: [:index]
+  def index
+    # Your existing code for the welcome page
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable,,:rememberable
   devise :database_authenticatable, :registerable,
-         :recoverable,:validatable 
- has_many :recipes
- has_many :inventories
+         :recoverable, :validatable
+  has_many :recipes
+  has_many :inventories
 end

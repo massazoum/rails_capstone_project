bfd5ba05de931c7960cf5300ae8d2f984dad850e
Isabella <isabellaotoo25@gmail.com>
2023-08-10 14:14:40 +0000

    Fix linters

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,41 +1,38 @@
 class InventoriesController < ApplicationController
-    before_action :authenticate_user!
-    
-    def index
-      @inventories = current_user.inventories
-    end
-  
-    def new
-      @inventory = Inventory.new
-    end
-  
-    def create
-      @inventory = current_user.inventories.new(inventory_params)
-  
-      if @inventory.save
-        redirect_to inventories_path, notice: 'Inventory was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def show
-      @inventory = current_user.inventories.includes(inventory_foods: :food).find(params[:id])
-      @inventory_food = @inventory.inventory_foods.new
-    end
-    
-    
-    def destroy
-      @inventory = current_user.inventories.find(params[:id])
-      @inventory.destroy
-      redirect_to inventory_path, notice: 'Inventory was successfully deleted.'
-    end
-  
-    private
+  before_action :authenticate_user!
 
-def inventory_params
-  params.require(:inventory).permit(:name, :description)
-end
-
+  def index
+    @inventories = current_user.inventories
   end
-  
+
+  def new
+    @inventory = Inventory.new
+  end
+
+  def create
+    @inventory = current_user.inventories.new(inventory_params)
+
+    if @inventory.save
+      redirect_to inventories_path, notice: 'Inventory was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @inventory = current_user.inventories.includes(inventory_foods: :food).find(params[:id])
+    @inventory_food = @inventory.inventory_foods.new
+  end
+
+  def destroy
+    @inventory = current_user.inventories.find(params[:id])
+    @inventory.destroy
+    redirect_to inventory_path, notice: 'Inventory was successfully deleted.'
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name, :description)
+  end
+end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,7 +1,7 @@
 class Inventory < ApplicationRecord
   belongs_to :user
   has_many :inventory_foods
-  
+
   def missing_food_items(recipe)
     missing_items = []
 
@@ -10,12 +10,12 @@ class Inventory < ApplicationRecord
       inventory_food = inventory_foods.find_by(food_id: recipe_food.id)
 
       # If the food is missing or the quantity is insufficient
-      if inventory_food.nil? || inventory_food.quantity < recipe_food.quantity
-        missing_items << {
-          food: recipe_food,
-          missing_quantity: recipe_food.quantity - (inventory_food&.quantity || 0)
-        }
-      end
+      next unless inventory_food.nil? || inventory_food.quantity < recipe_food.quantity
+
+      missing_items << {
+        food: recipe_food,
+        missing_quantity: recipe_food.quantity - (inventory_food&.quantity || 0)
+      }
     end
 
     missing_items

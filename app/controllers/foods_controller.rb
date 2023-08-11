a0e1f1b25a469a3f4c97d0ff28cf4db94c46f4f2
massa <zoumaniguimassa12@gmail.com>
2023-08-11 11:06:45 +0000

    Inventories , foods and inventories_food  controllers added

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,42 @@
+# app/controllers/foods_controller.rb
+class FoodsController < ApplicationController
+  before_action :set_food, only: [:show, :edit, :destroy]
+
+  def index
+    @foods = Food.all
+  end
+
+  def show
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @food.destroy
+    redirect_to foods_path, notice: 'Food was successfully deleted.'
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement, :price)
+  end
+end

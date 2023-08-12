4c8bc0ad2fb10bdf74e393d64842a0b327198057
massa <zoumaniguimassa12@gmail.com>
2023-08-12 01:25:32 +0000

    Tests added for requests

---
+++ spec/requests/foods_requests_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Food, type: :request do
+  describe ' foods route should be available' do
+    before(:each) do
+      @isabella = User.create!(name: 'Isabella',
+                               email: 'me@gmail.com',
+                               password: '1234567', password_confirmation: '1234567')
+      @shiro = Food.create(name: 'Shiro', measurement: 'KG', price: 45.99)
+    end
+    it 'should render the food index action correctly' do
+      sign_in @isabella
+      get '/foods'
+      expect(response).to be_successful
+      expect(response.body).to include('Shiro')
+    end
+
+    it 'should alow users to create foods' do
+      sign_in @isabella
+      get '/foods'
+      expect(response).to be_successful
+      expect(response.body).to include('New Food')
+    end
+  end
+end
